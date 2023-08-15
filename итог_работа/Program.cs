2fde487be13dfbeb4e44f8691c6d252b6c6d6140
Anna <99716@mail.ru>
2023-08-15 20:44:27 +0300

    Корректировка

--- итог_работа/Program.cs
+++ итог_работа/Program.cs
@@ -1,10 +1,8 @@
 ﻿Console.Write("Введите массив строк (в кавычках через пробел): ");
 string enter = Console.ReadLine();
-string[] StartArray = enter.Split(new char[] {' '}); 
-// Разделение элементов строки в массив
+string[] StartArray = enter.Split(new char[] {' '}); // Разделение элементов строки в массив
 int length = 3; // Параметр длины элемента первого массива
-string[] WithoutQuotesArray = StartArray.Select(str => str.Replace("\"", "")).ToArray();
-// Убираем кавычки из элементов массива
+string[] WithoutQuotesArray = StartArray.Select(str => str.Replace("\"", "")).ToArray(); // Убираем кавычки из элементов массива
 string[] FinishArray = WithoutQuotesArray.Where(str => str.Length <= length).ToArray();
 // Вывод элементов нового массива
 Console.WriteLine("Массив из строк, длина которых меньше, либо равна 3 символам: ");
